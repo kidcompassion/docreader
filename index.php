84db3d66ff7da2cdb14e03c9911d8ed59a713a30
kidcompassion <sallypoulsen@gmail.com>
2014-07-15 19:15:53 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('functions.php');
+
+$dir    = 'docs';
+$files1 = scandir($dir, -1);
+
+foreach ($files1 as $filename):
+	echo '<strong>';
+	$filename = 'docs/'.$filename;
+	echo $filename;
+	echo '</strong>';
+
+
+
+
+$zip = zip_open($filename);
+if (!$zip || is_numeric($zip)) return false;
+//echo zip_read($zip);
+if ($zip)
+  {
+  while ($zip_entry = zip_read($zip))
+    {
+    echo "<p>";
+    if ( zip_entry_name($zip_entry) == 'word/document.xml'):
+    if (zip_entry_open($zip, $zip_entry))
+      {
+      echo "File Contents:<br/>";
+
+      $contents = zip_entry_read($zip_entry);
+      echo "$contents<br />";
+      zip_entry_close($zip_entry);
+      }
+      endif;
+    echo "</p>";
+  }
+
+zip_close($zip);
+}
+
+endforeach;
+	
+
+
+
+
+
+;?>
