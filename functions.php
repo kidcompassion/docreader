1b477bf48bfa5630fe6646ca63e29911e4ca8293
kidcompassion <sallypoulsen@gmail.com>
2014-07-15 22:12:46 -0600

    Lays results out as a table.

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php 
+//gets the text between two specific words
  function between($src,$start,$end){
     $txt=explode($start,$src);
     $txt2=explode($end,$txt[1]);
--- index.php
+++ index.php
@@ -4,12 +4,25 @@ require_once('functions.php');
 
 $dir    = 'docs';
 $files1 = scandir($dir, -1);
+?>
+<table>
+<tbody>
+  <tr>
+    <th>FILENAME</th>
+    <th>LOCATION</th>
+    <th>AUTHOUR</th>
+    <th>DATE</th>
 
+
+  </tr>
+
+<?php
 foreach ($files1 as $filename):
-	echo '<strong>';
+	echo '<tr>';
+  echo '<td><strong>';
 	$filename = 'docs/'.$filename;
 	echo $filename;
-	echo '</strong>';
+	echo '</strong></td>';
 
 
 
@@ -17,32 +30,25 @@ foreach ($files1 as $filename):
 $zip = zip_open($filename);
 if (!$zip || is_numeric($zip)) return false;
 //echo zip_read($zip);
-if ($zip)
-  {
-  while ($zip_entry = zip_read($zip))
-    {
-    echo "<p>";
+if ($zip):
+  while ($zip_entry = zip_read($zip)):
     if ( zip_entry_name($zip_entry) == 'word/document.xml'):
-    if (zip_entry_open($zip, $zip_entry))
-      {
-      echo "File Contents:";
+      if (zip_entry_open($zip, $zip_entry)):
 
-      $contents = zip_entry_read($zip_entry);
-      echo '<p>' .$contents . '</p>';
-      zip_entry_close($zip_entry);
-      }
+        $contents = zip_entry_read($zip_entry);
+        $contents = strtolower($contents);
+
+        echo '<td>' . between($contents,'location','date') . '</td>';
+        echo '<td>' . between($contents,'authour','location') . '</td>';
+        echo '<td>' . between($contents,'date','end') . '</td>';
+        zip_entry_close($zip_entry);
+        endif;
       endif;
-    echo "</p>";
-  }
+  endwhile;
 
 zip_close($zip);
-}
+endif;
 
-endforeach;
-	
-
-
-
-
-
-;?>
+endforeach;?>
+</tbody>
+</table>
